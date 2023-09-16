493ed64e43ed5092e189499fba34e0728481744a
Michael Hofmair <michael@hofmair.net>
2023-09-16 09:59:54 +0200

    adds fidelity to the fix permission script

--- fixpermissions.sh
+++ fixpermissions.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-sudo chown 1000:1000 -R ../php;
+srcDir="../php/public"
+
+if [ ! -d "$srcDir" ]; then
+  mkdir -p "$srcDir"
+  echo "Created folder: $srcDir"
+else
+  echo "Folder $srcDir exists."
+fi
+echo "Fixing folder ownership $srcDir"
+sudo chown 1000:1000 -R $srcDir
+echo "OK"

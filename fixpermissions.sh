a91e585496b634d404489d8217ad833cac64a295
Michael Hofmair <michael@hofmair.net>
2023-09-22 17:43:44 +0200

    the fixpermission.sh now chowns the entire webroot ("php" folder)

--- fixpermissions.sh
+++ fixpermissions.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 srcDir="../php/public"
+webroot="../php"
 
 if [ ! -d "$srcDir" ]; then
  sudo  mkdir -p "$srcDir"
@@ -8,5 +9,5 @@ else
   echo "Folder $srcDir exists."
 fi
 echo "Fixing folder ownership $srcDir"
-sudo chown 1000:1000 -R $srcDir
+sudo chown 1000:1000 -R $webroot
 echo "OK"

a2818ae9d7fd62a74240b092b1079b7017e04b9d
Michael Hofmair <michael@hofmair.net>
2024-10-27 17:05:02 +0100

    changes deployscript do omitt the vendor & cache folder from syncing from dev to the deploytarget

--- deploy.sh
+++ deploy.sh
@@ -18,9 +18,12 @@ if [ -z "$DEPLOY_TARGET_IP" ] || [ -z "$DEPLOY_PATH" ] || [ -z "$DEPLOY_USER" ] 
 fi
 
 # Rsync the application code in php/ to the server, excluding dotfiles
-rsync -avz --delete --exclude='.*' ../php/ "$DEPLOY_TARGET_IP:$DEPLOY_PATH"
+rsync -avz --delete --exclude='.*'--exclude='vendor' --exclude='cache' ../php/ "$DEPLOY_TARGET_IP:$DEPLOY_PATH"
 
 # Set user and group permissions on the target server
 ssh "$DEPLOY_TARGET_IP" "echo $SUDO_PASS | sudo -S chown -R $DEPLOY_USER:$DEPLOY_GROUP $DEPLOY_PATH"
+
+# SSH into the server and run composer install in the deployed path
+ssh "$DEPLOY_TARGET_IP" "cd $DEPLOY_PATH && sudo -u www-data composer install --no-dev --optimize-autoloader"
 
 echo "Deployment to $DEPLOY_TARGET_IP:$DEPLOY_PATH completed with user $DEPLOY_USER and group $DEPLOY_GROUP."

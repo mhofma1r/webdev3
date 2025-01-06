33cdfebbca8a79c720d3e320c59e722cd12a8cea
Michael Hofmair <michael@hofmair.net>
2025-01-06 19:30:18 +0100

    fixed automated DB seeding

--- db_seed/seed.sh
+++ db_seed/seed.sh
@@ -4,15 +4,18 @@ ENV_FILE="/tmp/db_seed/.env"
 if [ -f "$ENV_FILE" ]; then
   # Source the file
   source "$ENV_FILE"
-  echo "Sourced $FILE_TO_SOURCE"
+  echo "Sourced Env"
 fi
-
-# Define database credentials and database name
-
-
 # Drop and recreate the database
-mysql -u "$DB_USER" -p"$DB_PASSWORD" -h "$DB_HOST" -e "
-DROP DATABASE IF EXISTS \`$DB_NAME\`;
-CREATE DATABASE \`$DB_NAME\` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+echo "drop previous db"
+/usr/bin/mariadb -u "$DB_USER" -p"$DB_PASSWORD" -h "$DB_HOST" -e "
+DROP DATABASE IF EXISTS $DB_NAME;
 "
-mysql -u syslog_user -p syslog < /tmp/db_import/db_seed.sql
+echo "recreate db"
+/usr/bin/mariadb -u "$DB_USER" -p"$DB_PASSWORD" -h "$DB_HOST" -e "
+CREATE DATABASE $DB_NAME;
+"
+echo "reimport database"
+/usr/bin/mariadb -u "$DB_USER" -p"$DB_PASSWORD" -h "$DB_HOST" -D "$DB_NAME" < /tmp/db_seed/db_seed.sql
+echo "reinforce FK's"
+/usr/bin/mariadb -u "$DB_USER" -p"$DB_PASSWORD" -h "$DB_HOST" -D "$DB_NAME" < /tmp/db_seed/1fk_events.sql;
